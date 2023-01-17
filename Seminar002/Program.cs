37915a742f9bf254fa4684133cb1be682cb957df
artketler <artketler.ak@gmail.com>
2023-01-17 20:32:59 +0300

    Seminar 002 P003 project

--- Seminar002/Program.cs
+++ Seminar002/Program.cs
@@ -3,7 +3,7 @@
 // 12-> 2 
 // 85 -> 8
 
-int number = new Random().Next(10, 100);
+int number = new Random().Next(100, 999);
 
 Console.WriteLine("Сгенерировано число: " + number);
 
@@ -19,9 +19,27 @@ else
 
 Console.WriteLine("Наибольшая цифра в числе " + number + ": " + maxDigit);
 
-
+//Пример 2
 
 if(int.TryParse(Console.ReadLine(), out int number))
     Console.WriteLine("Прочитали число: " + number);
 else
     Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+
+
+
+
+
+// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int num = new Random().Next(100, 1000);
+
+int num1 = (num / 100) * 10;
+int num3 = num % 10;
+
+Console.WriteLine($"Начальное число {num}. Результат после удаления второй цифры: {num1 + num3}");
+
+
