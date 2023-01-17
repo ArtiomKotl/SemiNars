a4f6481ec7438dd64d0b6f3b27ecabedfe53e665
artketler <artketler.ak@gmail.com>
2023-01-17 21:25:04 +0300

    Seminar 002 P005 project

--- Seminar002/Program.cs
+++ Seminar002/Program.cs
@@ -1,87 +1,20 @@
-﻿// 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
-// 78 -> 8 
-// 12-> 2 
-// 85 -> 8
+﻿//  14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+//14 -> нет 
+//46 -> нет 
+//161 -> да 
 
-int number = new Random().Next(100, 999);
-
-Console.WriteLine("Сгенерировано число: " + number);
-
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-
-int maxDigit = 0;
-
-if (firstDigit > secondDigit)
-    maxDigit = firstDigit;
-else
-    maxDigit = secondDigit;
-
-Console.WriteLine("Наибольшая цифра в числе " + number + ": " + maxDigit);
-
-//Пример 2
-
-if(int.TryParse(Console.ReadLine(), out int number))
-    Console.WriteLine("Прочитали число: " + number);
-else
-    Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
-
-
-
-
-
-// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
-//456 -> 46
-//782 -> 72
-//918 -> 98
-
-int num = new Random().Next(100, 1000);
-
-int num1 = (num / 100) * 10;
-int num3 = num % 10;
-
-Console.WriteLine($"Начальное число {num}. Результат после удаления второй цифры: {num1 + num3}");
-
-
-// 12. 
-
-
-Console.Write("Введите первое число: ");
-int numOne = Convert.ToInt32( Console.ReadLine() );  
-
-Console.Write("Введите второе число: ");
-int numTwo = Convert.ToInt32( Console.ReadLine() );
-int result = numOne % numTwo;
-
-if (result == 0)
+int number1;
+while(number1 == 0) 
 {
-    System.Console.WriteLine("кратно");
+Console.WriteLine("Введите число");
+ number1 = Convert.ToInt32(Console.ReadLine());
 }
-else
+if (number1 == 0)
 {
-    System.Console.WriteLine($"Не кратно, остаток {result}");
+  System.Console.WriteLine("Число является нулём");
+  return;
 }
-
-
-// Вариант
-
-Console.WriteLine("Введите первое число");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int number2 = Convert.ToInt32(Console.ReadLine());
-
-if (number1 == 0 || number2 == 0) {
-    System.Console.WriteLine("Одно из чисел является нулём");
-    return;
-}
-
-
-int result = number1 % number2;
-
-if (result == 0)     
-    Console.WriteLine("кратно"); 
-else     
-    Console.WriteLine("не кратно, остаток " + result);
-
-
-    
+if (number1 % 7 == 0 && number1 % 23 == 0)
+  Console.WriteLine("Да");
+else
+  Console.WriteLine("нет");
