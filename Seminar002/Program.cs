1488ae9ffbba70accce8f11baf947546e876c93c
artketler <artketler.ak@gmail.com>
2023-01-17 20:16:00 +0300

    Seminar 002 P001 project

--- Seminar002/Program.cs
+++ Seminar002/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+
+Console.WriteLine("Сгенерировано число: " + number);
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+int maxDigit = 0;
+
+if (firstDigit > secondDigit)
+    maxDigit = firstDigit;
+else
+    maxDigit = secondDigit;
+
+Console.WriteLine("Наибольшая цифра в числе " + number + ": " + maxDigit);
